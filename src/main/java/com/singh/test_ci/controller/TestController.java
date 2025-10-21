8c34f0945f764ceaa9daa8b7691ab229615207e0
unknown <vineetsinghHub>
2025-10-21 16:13:56 +0530

    initial setup with an api for test

---
+++ src/main/java/com/singh/test_ci/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.singh.test_ci.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping
+    public String test(){
+        return "Test Controller --> test()";
+    }
+
+}
---
+++ src/test/java/com/singh/test_ci/TestCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singh.test_ci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
